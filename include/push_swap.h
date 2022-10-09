2e62d076b2e6df8fbbca5b02b967697e21e2a793
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-10-09 12:50:02 -0300

    feat: add stack operations

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 18:45:10 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/10/09 10:17:53 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/10/09 12:47:38 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,16 @@ typedef struct s_stack
 }	t_stack;
 
 int		*parse_int(int size, char **input);
+long	ft_atol(const char *nptr);
+void	err_exit(void);
+
 t_stack	*init_stack(int *input, int size);
 void	create_index(t_stack **stack, int *input, int size);
 void	destroy_stack(t_stack **top);
-long	ft_atol(const char *nptr);
-void	err_exit(void);
+void	push(t_stack **stack, t_stack *item);
+t_stack	*pop(t_stack **stack);
+void	push_bottom(t_stack **stack, t_stack *item);
+t_stack	*pop_bottom(t_stack **stack);
+int		stack_size(t_stack *stack);
 
 #endif
---
+++ src/stack_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 20:29:25 by mdias-ma          #+#    #+#             */
+/*   Updated: 2022/10/09 12:45:34 by mdias-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	push(t_stack **stack, t_stack *item)
+{
+	item->next = *stack;
+	*stack = item;
+}
+
+t_stack	*pop(t_stack **stack)
+{
+	t_stack	*temp;
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	return (temp);
+}
+
+void	push_bottom(t_stack **stack, t_stack *item)
+{
+	t_stack	*temp;
+
+	temp = *stack;
+	while (temp->next)
+		temp = temp->next;
+	item->next = NULL;
+	temp->next = item;
+}
+
+t_stack	*pop_bottom(t_stack **stack)
+{
+	t_stack	*temp;
+	t_stack	*last;
+	int		size;
+
+	size = stack_size(*stack) - 2;
+	temp = *stack;
+	while (size--)
+		temp = temp->next;
+	last = temp->next;
+	temp->next = NULL;
+	return (last);
+}
+
+int	stack_size(t_stack *stack)
+{
+	int	size;
+
+	size = 0;
+	while (stack != NULL)
+	{
+		stack = stack->next;
+		size++;
+	}
+	return (size);
+}
