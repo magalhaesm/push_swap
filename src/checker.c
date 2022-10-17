6ee88aa3cd59d1e8188767ceb7f4ac2bd8760dee
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-10-17 13:24:53 -0300

    refactor: remove unneed code

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 17:49:07 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/10/17 11:53:57 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/10/17 13:24:30 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,7 @@ int	main(int argc, char **argv)
 	if (size <= 1)
 		return (EXIT_SUCCESS);
 	numbers = parse_int(size, argv + 1);
-	if (numbers == NULL)
-		err_exit(EXIT_FAILURE);
 	a = init_stack(numbers, size);
-	if (a == NULL)
-		err_exit(EXIT_FAILURE);
 	b = NULL;
 	create_index(&a, numbers, size);
 	free(numbers);
